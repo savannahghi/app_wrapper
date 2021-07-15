ceef1589d66db02d83ee07f756602657f52717c7
Abdi Adan <adanabdi036@gmail.com>
2021-07-15 12:51:10 +0300

    chore: documentation fixes (#4)

--- lib/src/app_wrapper.dart
+++ lib/src/app_wrapper.dart
@@ -3,11 +3,18 @@ library app_wrapper;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:app_wrapper/app_wrapper.dart';
-
 import 'package:flutter_graphql_client/graph_client.dart';
 
-class SILAppWrapper extends StatefulWidget {
-  const SILAppWrapper({
+/// [AppWrapper] Is a top level Class used to wrap your application.
+/// 
+/// @params
+/// - `child` the widget that will be wrapped by this wrapper
+/// - `appName` A required string that identifies your application
+/// - `graphQLClient` An instance of [ISILGraphQlClient] which is the blueprint of a valid GraphQL client.
+/// - `appContexts` [context] app running environments.
+/// - `eventBus` Used to log events
+class AppWrapper extends StatefulWidget {
+  const AppWrapper({
     required this.child,
     required this.appName,
     required this.graphQLClient,
@@ -24,7 +31,7 @@ class SILAppWrapper extends StatefulWidget {
   /// Used to send events to the backend
   final dynamic eventBus;
 
-  /// [graphQLClient] is the graphql client from sil_graphql_client package
+  /// [graphQLClient] is the graphql client from flutter_graphql_client package
   /// it is dynamic to avoid cyclic-dependency errors when packages
   /// cross depend on each other
   final ISILGraphQlClient graphQLClient;
@@ -40,10 +47,10 @@ class SILAppWrapper extends StatefulWidget {
   final List<AppContext> appContexts;
 
   @override
-  SILAppWrapperState createState() => SILAppWrapperState();
+  AppWrapperState createState() => AppWrapperState();
 }
 
-class SILAppWrapperState extends State<SILAppWrapper> {
+class AppWrapperState extends State<AppWrapper> {
   @override
   void initState() {
     super.initState();
@@ -59,7 +66,7 @@ class SILAppWrapperState extends State<SILAppWrapper> {
     return LayoutBuilder(
       key: const Key('sil_app_wrapper'),
       builder: (BuildContext context, BoxConstraints boxConstraints) {
-        return SILAppWrapperBase(
+        return AppWrapperBase(
           appName: widget.appName,
           eventBus: widget.eventBus,
           graphQLClient: widget.graphQLClient,
