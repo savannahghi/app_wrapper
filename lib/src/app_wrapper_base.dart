a1f5d8172ce87972e7d534c725b6ffb6d9a8a8e3
Kevin Maiyo <47208315+maiyo01@users.noreply.github.com>
2021-07-21 13:08:29 +0300

    chore: update graphql client (#6)

--- lib/src/app_wrapper_base.dart
+++ lib/src/app_wrapper_base.dart
@@ -2,10 +2,10 @@ import 'package:flutter/widgets.dart';
 import 'package:app_wrapper/app_wrapper.dart';
 import 'package:flutter_graphql_client/graph_client.dart';
 
-/// [AppWrapperBase] is a top level base class used to efficiently propagate information down the tree. 
-/// 
+/// [AppWrapperBase] is a top level base class used to efficiently propagate information down the tree.
+///
 /// It extends [InheritedWidget] and provides consumers with additional data about the device the apps are running on.
-/// 
+///
 /// Consumers will rebuild when the state of this widget changes.
 ///
 /// @params
@@ -13,7 +13,7 @@ import 'package:flutter_graphql_client/graph_client.dart';
 /// It provides out of the box implementation of client methods that can be overridden.
 /// - `appName` A required string that identifies your application.
 /// - `appContexts` [context] app running environments.
-/// - `deviceCapabilities` An instance of [IDeviceCapabilities] that is used to check for Device Camera 
+/// - `deviceCapabilities` An instance of [IDeviceCapabilities] that is used to check for Device Camera
 /// and Device BiometricType (face or fingerprint).
 /// - `eventBus` Used to log events
 /// - `shouldNotify` bool used to determine whether the framework should notify widgets that inherit from this widget.
@@ -38,7 +38,7 @@ class AppWrapperBase extends InheritedWidget {
   /// [graphQLClient] is the graphql client from flutter_graphql_client package
   /// it is dynamic to avoid cyclic-dependency errors when packages
   /// cross depend on each other
-  final ISILGraphQlClient graphQLClient;
+  final IGraphQlClient graphQLClient;
 
   /// [context] is the app running environments
   final List<AppContext> appContexts;
